fedd0d5153d1906abaf71f49e8e14bb78908a368
DuHuDiCo <dudicodiaz@gmail.com>
2024-11-07 09:28:07 -0500

    commit

---
+++ src/main/java/com/apppn/apppn/ApppnApplication.java
@@ -0,0 +1,13 @@
+package com.apppn.apppn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApppnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApppnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apppn/apppn/ApppnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apppn.apppn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApppnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
