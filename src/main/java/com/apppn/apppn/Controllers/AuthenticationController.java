99020cc5276c99f910ee9a497f60dde08d8ee3d9
DuHuDiCo <dudicodiaz@gmail.com>
2024-11-13 12:00:39 -0500

    commit

--- src/main/java/com/apppn/apppn/Controllers/AuthenticationController.java
+++ src/main/java/com/apppn/apppn/Controllers/AuthenticationController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -56,7 +57,7 @@ public class AuthenticationController {
         return googleAuthenticationService.getGoogleAuthentication();
     }
 
-    @GetMapping("/movil")
+    @PostMapping("/movil")
     public ResponseEntity<?> verifyTokenAndGetUserProfile(@RequestBody Map<String, String> request) throws GeneralSecurityException, IOException {
         return googleAuthenticationService.verifyTokenAndGetUserProfile(request.get("access_token"));
     }
