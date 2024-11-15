6278c757b87d583cae58fb12eeca11a09a68926d
DuHuDiCo <dudicodiaz@gmail.com>
2024-11-15 10:53:06 -0500

    commit

--- src/main/java/com/apppn/apppn/Security/Security/MySecurityConfiguration.java
+++ src/main/java/com/apppn/apppn/Security/Security/MySecurityConfiguration.java
@@ -61,7 +61,7 @@ public class MySecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.csrf().disable()
         .cors().and()
         .authorizeRequests()
-                .antMatchers("/api/v1/auth/**", "/api/v1/google/callback", "/api/v1/google/authorize")
+                .antMatchers("/api/v1/auth/**", "/api/v1/google/callback", "/api/v1/google/authorize", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/actuator/**")
                 .permitAll()
                 .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .anyRequest().authenticated()
