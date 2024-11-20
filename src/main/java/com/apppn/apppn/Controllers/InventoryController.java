50c7a41e3e9e6ecea6829afad592203d3079175e
DuHuDiCo <dudicodiaz@gmail.com>
2024-11-20 12:25:25 -0500

    commit

---
+++ src/main/java/com/apppn/apppn/Controllers/InventoryController.java
@@ -0,0 +1,41 @@
+package com.apppn.apppn.Controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apppn.apppn.Exceptions.ErrorResponse;
+import com.apppn.apppn.Models.Inventory;
+import com.apppn.apppn.Service.InventoryService;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+@RestController
+@RequestMapping("/api/v1/inventory")
+public class InventoryController {
+
+
+    private final InventoryService inventoryService;
+
+    public InventoryController(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = Inventory.class))),
+            @ApiResponse(responseCode = "400", description = "BAD_REQUEST", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            
+    })
+    @GetMapping("/byUser")
+    public ResponseEntity<?> getInventories(@RequestParam("idUser") Long idUser) {
+        return inventoryService.getInventories(idUser);
+    }
+
+    
+
+}
