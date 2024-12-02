86c09f704f2e3350cc981298992436cc882701b7
DuHuDiCo <dudicodiaz@gmail.com>
2024-12-02 08:51:24 -0500

    commit

--- src/main/java/com/apppn/apppn/Controllers/LiquidacionController.java
+++ src/main/java/com/apppn/apppn/Controllers/LiquidacionController.java
@@ -1,6 +1,7 @@
 package com.apppn.apppn.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +32,7 @@ public class LiquidacionController {
             @ApiResponse(responseCode = "400", description = "BAD_REQUEST", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
             @ApiResponse(responseCode = "404", description = "NOT_FOUND", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
     })
-    @GetMapping("/")
+    @PostMapping("/")
     public Object saveLiquidacion(@RequestBody LiquidacionDTO liquidacionDTO) {
         return liquidacionService.saveLiquidacion(liquidacionDTO);
     }
