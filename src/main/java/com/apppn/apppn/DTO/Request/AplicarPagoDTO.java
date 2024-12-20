cd1c5a56841d46486a87df84a3977cacf61ff15f
DuHuDiCo <dudicodiaz@gmail.com>
2024-12-20 15:40:07 -0500

    commi

--- src/main/java/com/apppn/apppn/DTO/Request/AplicarPagoDTO.java
+++ src/main/java/com/apppn/apppn/DTO/Request/AplicarPagoDTO.java
@@ -1,12 +1,15 @@
 package com.apppn.apppn.DTO.Request;
 
+import java.util.Date;
 
 public class AplicarPagoDTO {
 
     private Long idPlanPago;
     private Long valor;
     private Long idFacturacion;
+    private Date fechaPago;
 
+    
 
     public AplicarPagoDTO() {
     }
@@ -39,6 +42,16 @@ public class AplicarPagoDTO {
 
     public void setIdFacturacion(Long idFacturacion) {
         this.idFacturacion = idFacturacion;
+    }
+
+
+    public Date getFechaPago() {
+        return fechaPago;
+    }
+
+
+    public void setFechaPago(Date fechaPago) {
+        this.fechaPago = fechaPago;
     }
     
     
--- src/main/java/com/apppn/apppn/ServiceImpl/AbonosServiceImpl.java
+++ src/main/java/com/apppn/apppn/ServiceImpl/AbonosServiceImpl.java
@@ -1,11 +1,20 @@
 package com.apppn.apppn.ServiceImpl;
 
-import org.checkerframework.checker.units.qual.radians;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import com.apppn.apppn.DTO.Request.AplicarPagoDTO;
 import com.apppn.apppn.DTO.Request.PagoClienteDTO;
+import com.apppn.apppn.Exceptions.ErrorResponse;
+import com.apppn.apppn.Models.Facturacion;
+import com.apppn.apppn.Models.PlanPagos;
+import com.apppn.apppn.Repository.FacturacionRepository;
 import com.apppn.apppn.Repository.PlanPagosRepository;
 import com.apppn.apppn.Service.AbonoService;
 
@@ -14,15 +23,12 @@ public class AbonosServiceImpl implements AbonoService {
 
 
     private final PlanPagosRepository planPagosRepository;
-
+    private final FacturacionRepository facturacionRepository;
     
-
-
-    public AbonosServiceImpl(PlanPagosRepository planPagosRepository) {
+    public AbonosServiceImpl(PlanPagosRepository planPagosRepository, FacturacionRepository facturacionRepository) {
         this.planPagosRepository = planPagosRepository;
+        this.facturacionRepository = facturacionRepository;
     }
-
-
 
 
     @Override
@@ -32,7 +38,12 @@ public class AbonosServiceImpl implements AbonoService {
        }
 
        for (AplicarPagoDTO aplicarPagoDTO : pagoClientesDto.getAplicarPagoDTO()) {
-        
+            Facturacion facturacion = facturacionRepository.findById(aplicarPagoDTO.getIdFacturacion()).orElseThrow(() -> new IllegalArgumentException("No existe la facturacion con id " + aplicarPagoDTO.getIdFacturacion()));
+            if(Objects.isNull(facturacion)) {
+                throw new IllegalArgumentException("No existe la facturacion con id " + aplicarPagoDTO.getIdFacturacion());
+            }
+
+            List<PlanPagos> planPagos = facturacion.getPlanPagos().stream().filter(pp -> pp.getFechaPago().equals(aplicarPagoDTO.getFechaPago()) && pp.getSaldo()>0).collect(Collectors.toList());
        }
     }
 
