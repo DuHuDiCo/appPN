54f4b308a6ab1ba42eb626296470334a35d479b1
DuHuDiCo <dudicodiaz@gmail.com>
2025-01-03 10:37:09 -0500

    commit

--- src/main/java/com/apppn/apppn/Controllers/PagosClientesController.java
+++ src/main/java/com/apppn/apppn/Controllers/PagosClientesController.java
@@ -1,5 +1,9 @@
 package com.apppn.apppn.Controllers;
 
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +13,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.apppn.apppn.DTO.Request.AplicarPagoDTO;
 import com.apppn.apppn.DTO.Request.PagoClienteDTO;
 import com.apppn.apppn.Exceptions.ErrorResponse;
 import com.apppn.apppn.Models.Client;
 import com.apppn.apppn.Models.PagoClientes;
 import com.apppn.apppn.Service.PagosClientesService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -24,7 +32,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 @RestController
 @RequestMapping("/api/v1/pagosClientes")
 public class PagosClientesController {
-
 
     private final PagosClientesService pagosClientesService;
 
@@ -39,8 +46,25 @@ public class PagosClientesController {
             @ApiResponse(responseCode = "409", description = "CONFLICT", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
     })
     @PostMapping("/")
-    public ResponseEntity<?> crearPagoClientes(@RequestPart("pagoClientesDto") PagoClienteDTO pagoClientesDto) {
-        return pagosClientesService.crearPagoClientes(pagoClientesDto);
+    public ResponseEntity<?> crearPagoClientes(@RequestPart("pagoClientesDto") String pagoClientesDtoJson,
+            @RequestPart("pagoClientesDto.file") MultipartFile file) {
+        ObjectMapper mapper = new ObjectMapper();
+        PagoClienteDTO request = null;
+        try {
+            request = mapper.readValue(pagoClientesDtoJson,
+                    PagoClienteDTO.class);
+
+            // Asignar el archivo al objeto request
+            request.setComprobante(file);
+        } catch (JsonProcessingException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ErrorResponse(e.getMessage()));
+        }
+        if (Objects.isNull(request)) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("ERROR DE PARSEO EN DOCUMENTACION GENERAL REQUEST");
+        }
+        return pagosClientesService.crearPagoClientes(request);
     }
 
     @ApiResponses(value = {
