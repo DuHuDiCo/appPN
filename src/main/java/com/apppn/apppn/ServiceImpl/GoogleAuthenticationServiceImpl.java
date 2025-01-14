11008434dcb312c671e6821935e31c6ae79f85b7
DuHuDiCo <dudicodiaz@gmail.com>
2025-01-13 21:27:18 -0500

    commit

--- src/main/java/com/apppn/apppn/ServiceImpl/GoogleAuthenticationServiceImpl.java
+++ src/main/java/com/apppn/apppn/ServiceImpl/GoogleAuthenticationServiceImpl.java
@@ -1,221 +1,183 @@
 package com.apppn.apppn.ServiceImpl;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.security.GeneralSecurityException;
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.view.RedirectView;
-
-import com.apppn.apppn.DTO.Request.UserDTO;
-import com.apppn.apppn.DTO.Request.UserProfileGoogle;
-import com.apppn.apppn.Exceptions.ErrorResponse;
-import com.apppn.apppn.Models.User;
-import com.apppn.apppn.Service.GoogleAuthenticationService;
-import com.apppn.apppn.Service.UserService;
-import com.apppn.apppn.Utils.Functions;
-import com.google.api.client.auth.oauth2.TokenResponse;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.googleapis.auth.oauth2.GoogleRefreshTokenRequest;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpRequestFactory;
-import com.google.api.client.http.HttpResponse;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 
 @Service
-public class GoogleAuthenticationServiceImpl implements GoogleAuthenticationService {
+public class GoogleAuthenticationServiceImpl {
 
-    @Value("${google.clientId}")
-    private String clientId;
+    // @Value("${google.clientId}")
+    // private String clientId;
 
-    @Value("${google.clientIdMovil}")
-    private String clientIdMovil;
+    // @Value("${google.clientIdMovil}")
+    // private String clientIdMovil;
 
-    @Value("${google.client-secret}")
-    private String clientSecret;
+    // @Value("${google.client-secret}")
+    // private String clientSecret;
 
-    @Value("${url-back}")
-    private String urlBackend;
+    // @Value("${url-back}")
+    // private String urlBackend;
 
-    private final String APPLICATION_NAME;
-    private final JsonFactory JSON_FACTORY;
-    private final List<String> SCOPE;
-    private final UserService userService;
-    private final Functions functions;
+    // private final String APPLICATION_NAME;
+    // private final JsonFactory JSON_FACTORY;
+    // private final List<String> SCOPE;
+    // private final UserService userService;
+    // private final Functions functions;
 
-    public GoogleAuthenticationServiceImpl(UserService userService, Functions functions) {
-        this.userService = userService;
-        APPLICATION_NAME = "APPPN";
-        JSON_FACTORY = JacksonFactory.getDefaultInstance();
-        SCOPE = Arrays.asList("https://www.googleapis.com/auth/userinfo.profile",
-                "https://www.googleapis.com/auth/userinfo.email",
-                "openid");
-        this.functions = functions;
-    }
+    // public GoogleAuthenticationServiceImpl(UserService userService, Functions functions) {
+    //     this.userService = userService;
+    //     APPLICATION_NAME = "APPPN";
+    //     JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    //     SCOPE = Arrays.asList("https://www.googleapis.com/auth/userinfo.profile",
+    //             "https://www.googleapis.com/auth/userinfo.email",
+    //             "openid");
+    //     this.functions = functions;
+    // }
 
-    @Override
-    public RedirectView getGoogleAuthentication() {
-        StringBuilder authorizationUrl = new StringBuilder();
+    // @Override
+    // public RedirectView getGoogleAuthentication() {
+    //     StringBuilder authorizationUrl = new StringBuilder();
 
-        authorizationUrl.append("https://accounts.google.com/o/oauth2/auth?response_type=code&client_id="
-                .concat(clientId)
-                .concat("&redirect_uri=".concat(urlBackend).concat("/api/v1/google/callback&")
-                        .concat("scope=" + String.join("%20",
-                                Arrays.asList("https://www.googleapis.com/auth/userinfo.profile",
-                                        "https://www.googleapis.com/auth/userinfo.email",
-                                        "openid"))
-                                .concat("&access_type=offline&prompt=consent"))));
+    //     authorizationUrl.append("https://accounts.google.com/o/oauth2/auth?response_type=code&client_id="
+    //             .concat(clientId)
+    //             .concat("&redirect_uri=".concat(urlBackend).concat("/api/v1/google/callback&")
+    //                     .concat("scope=" + String.join("%20",
+    //                             Arrays.asList("https://www.googleapis.com/auth/userinfo.profile",
+    //                                     "https://www.googleapis.com/auth/userinfo.email",
+    //                                     "openid"))
+    //                             .concat("&access_type=offline&prompt=consent"))));
 
-        return new RedirectView(authorizationUrl.toString());
-    }
+    //     return new RedirectView(authorizationUrl.toString());
+    // }
 
-    @Override
-    public GoogleClientSecrets createGoogleClientSecrets(String clientId, String clientSecret) {
-        GoogleClientSecrets.Details details = new GoogleClientSecrets.Details();
-        details.setClientId(clientId);
-        details.setClientSecret(clientSecret);
-        GoogleClientSecrets clientSecrets = new GoogleClientSecrets();
-        clientSecrets.setWeb(details);
-        return clientSecrets;
-    }
+    // @Override
+    // public GoogleClientSecrets createGoogleClientSecrets(String clientId, String clientSecret) {
+    //     GoogleClientSecrets.Details details = new GoogleClientSecrets.Details();
+    //     details.setClientId(clientId);
+    //     details.setClientSecret(clientSecret);
+    //     GoogleClientSecrets clientSecrets = new GoogleClientSecrets();
+    //     clientSecrets.setWeb(details);
+    //     return clientSecrets;
+    // }
 
-    @Override
-    public ResponseEntity<?> googleCallBack(String code, List<String> scope)
-            throws IOException, GeneralSecurityException {
+    // @Override
+    // public ResponseEntity<?> googleCallBack(String code, List<String> scope)
+    //         throws IOException, GeneralSecurityException {
 
-        try {
-            GoogleClientSecrets clientSecrets = createGoogleClientSecrets(clientId, clientSecret);
-            GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                    GoogleNetHttpTransport.newTrustedTransport(), JSON_FACTORY, clientSecrets, scope)
-                    .setAccessType("offline").build();
+    //     try {
+    //         GoogleClientSecrets clientSecrets = createGoogleClientSecrets(clientId, clientSecret);
+    //         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+    //                 GoogleNetHttpTransport.newTrustedTransport(), JSON_FACTORY, clientSecrets, scope)
+    //                 .setAccessType("offline").build();
 
-            TokenResponse tokenResponse = flow.newTokenRequest(code)
-                    .setRedirectUri(urlBackend.concat("/api/v1/google/callback"))
-                    .execute();
+    //         TokenResponse tokenResponse = flow.newTokenRequest(code)
+    //                 .setRedirectUri(urlBackend.concat("/api/v1/google/callback"))
+    //                 .execute();
 
-            System.out.println("Token access Inicial: " + tokenResponse.getAccessToken());
-            System.out.println("Token refresh inicial: " + tokenResponse.getRefreshToken());
-            System.out.println("Expiration: " + tokenResponse.getExpiresInSeconds());
+    //         System.out.println("Token access Inicial: " + tokenResponse.getAccessToken());
+    //         System.out.println("Token refresh inicial: " + tokenResponse.getRefreshToken());
+    //         System.out.println("Expiration: " + tokenResponse.getExpiresInSeconds());
 
-            TokenResponse newTokenResponse = refreshAccessToken(tokenResponse.getRefreshToken());
+    //         TokenResponse newTokenResponse = refreshAccessToken(tokenResponse.getRefreshToken());
 
-            UserProfileGoogle profileInfo = getUserProfileInfo(newTokenResponse.getAccessToken());
+    //         UserProfileGoogle profileInfo = getUserProfileInfo(newTokenResponse.getAccessToken());
 
-            System.out.println("Email Profile: " + profileInfo.getEmail());
+    //         System.out.println("Email Profile: " + profileInfo.getEmail());
 
-            ResponseEntity<?> user = userService.getUserByEmail(profileInfo.getEmail());
-            if (user.getStatusCode().equals(HttpStatus.BAD_REQUEST)) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new RedirectView("https://www.gov.uk/404"));
-            }
-            User userFound = (User) user.getBody();
-            if (Objects.isNull(userFound)) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new RedirectView("https://www.gov.uk/404"));
-            }
+    //         ResponseEntity<?> user = userService.getUserByEmail(profileInfo.getEmail());
+    //         if (user.getStatusCode().equals(HttpStatus.BAD_REQUEST)) {
+    //             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new RedirectView("https://www.gov.uk/404"));
+    //         }
+    //         User userFound = (User) user.getBody();
+    //         if (Objects.isNull(userFound)) {
+    //             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new RedirectView("https://www.gov.uk/404"));
+    //         }
 
-            UserDTO userDTO = new UserDTO();
-            userDTO.setEmail(profileInfo.getEmail());
-            userDTO.setLastname(profileInfo.getGiven_name());
-            userDTO.setName(profileInfo.getFamily_name());
+    //         UserDTO userDTO = new UserDTO();
+    //         userDTO.setEmail(profileInfo.getEmail());
+    //         userDTO.setLastname(profileInfo.getGiven_name());
+    //         userDTO.setName(profileInfo.getFamily_name());
 
 
-            ResponseEntity<?> userEditResponse = userService.editUser(userFound.getIdUser(), userDTO);
+    //         ResponseEntity<?> userEditResponse = userService.editUser(userFound.getIdUser(), userDTO);
 
-            if (userEditResponse.getStatusCode().equals(HttpStatus.BAD_REQUEST)) {
-                return null;
-            }
+    //         if (userEditResponse.getStatusCode().equals(HttpStatus.BAD_REQUEST)) {
+    //             return null;
+    //         }
 
-            return ResponseEntity.status(HttpStatus.OK).body(new RedirectView("https://rentaraiz.duckdns.org/"));
+    //         return ResponseEntity.status(HttpStatus.OK).body(new RedirectView("https://rentaraiz.duckdns.org/"));
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println(e.getMessage());
-            return ResponseEntity.status(HttpStatus.OK).body(new RedirectView("https://www.gov.uk/404"));
-        }
+    //     } catch (Exception e) {
+    //         e.printStackTrace();
+    //         System.out.println(e.getMessage());
+    //         return ResponseEntity.status(HttpStatus.OK).body(new RedirectView("https://www.gov.uk/404"));
+    //     }
 
-    }
+    // }
 
-    private UserProfileGoogle getUserProfileInfo(String accessToken) throws IOException {
-        HttpTransport httpTransport = new NetHttpTransport();
-        HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
+    // private UserProfileGoogle getUserProfileInfo(String accessToken) throws IOException {
+    //     HttpTransport httpTransport = new NetHttpTransport();
+    //     HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
 
-        GenericUrl url = new GenericUrl("https://www.googleapis.com/oauth2/v3/userinfo");
-        url.put("access_token", accessToken);
+    //     GenericUrl url = new GenericUrl("https://www.googleapis.com/oauth2/v3/userinfo");
+    //     url.put("access_token", accessToken);
 
-        HttpResponse response = requestFactory.buildGetRequest(url).execute();
+    //     HttpResponse response = requestFactory.buildGetRequest(url).execute();
 
-        String responseBody;
-        try (InputStream content = response.getContent()) {
-            responseBody = new String(content.readAllBytes(), StandardCharsets.UTF_8);
-        }
+    //     String responseBody;
+    //     try (InputStream content = response.getContent()) {
+    //         responseBody = new String(content.readAllBytes(), StandardCharsets.UTF_8);
+    //     }
 
-        // Imprime el contenido de la respuesta para verificar el JSON
-        System.out.println("Response Body: " + responseBody);
+    //     // Imprime el contenido de la respuesta para verificar el JSON
+    //     System.out.println("Response Body: " + responseBody);
 
-        // Usa Gson para analizar la respuesta JSON en un objeto UserProfileGoogle
-        Gson gson = new Gson();
-        try {
-            UserProfileGoogle profileInfo = gson.fromJson(responseBody, UserProfileGoogle.class);
-            System.out.println("Parsed User Profile: " + profileInfo.getEmail()); // Verifica un campo
-            return profileInfo;
-        } catch (JsonSyntaxException e) {
-            e.printStackTrace();
-            System.out.println("Error parsing JSON to UserProfileGoogle: " + e.getMessage());
-            return null;
-        }
-    }
+    //     // Usa Gson para analizar la respuesta JSON en un objeto UserProfileGoogle
+    //     Gson gson = new Gson();
+    //     try {
+    //         UserProfileGoogle profileInfo = gson.fromJson(responseBody, UserProfileGoogle.class);
+    //         System.out.println("Parsed User Profile: " + profileInfo.getEmail()); // Verifica un campo
+    //         return profileInfo;
+    //     } catch (JsonSyntaxException e) {
+    //         e.printStackTrace();
+    //         System.out.println("Error parsing JSON to UserProfileGoogle: " + e.getMessage());
+    //         return null;
+    //     }
+    // }
 
-    @Override
-    public ResponseEntity<?> verifyTokenAndGetUserProfile(String accessToken)
-            throws GeneralSecurityException, IOException {
-        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(GoogleNetHttpTransport.newTrustedTransport(),
-                JSON_FACTORY)
-                .setAudience(Arrays.asList(clientId, clientIdMovil))
-                .build();
+    // @Override
+    // public ResponseEntity<?> verifyTokenAndGetUserProfile(String accessToken)
+    //         throws GeneralSecurityException, IOException {
+    //     GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(GoogleNetHttpTransport.newTrustedTransport(),
+    //             JSON_FACTORY)
+    //             .setAudience(Arrays.asList(clientId, clientIdMovil))
+    //             .build();
 
-        GoogleIdToken googleIdToken = verifier.verify(accessToken);
-        if (googleIdToken != null) {
+    //     GoogleIdToken googleIdToken = verifier.verify(accessToken);
+    //     if (googleIdToken != null) {
 
-            GoogleIdToken.Payload payload = googleIdToken.getPayload();
+    //         GoogleIdToken.Payload payload = googleIdToken.getPayload();
 
-            UserProfileGoogle userProfile = new UserProfileGoogle();
+    //         UserProfileGoogle userProfile = new UserProfileGoogle();
 
-            userProfile.setEmail(payload.getEmail());
-            userProfile.setName((String) payload.get("name"));
-            userProfile.setPicture((String) payload.get("picture"));
+    //         userProfile.setEmail(payload.getEmail());
+    //         userProfile.setName((String) payload.get("name"));
+    //         userProfile.setPicture((String) payload.get("picture"));
 
-            return ResponseEntity.status(HttpStatus.OK).body(userProfile);
-        } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("Invalid token"));
-        }
-    }
+    //         return ResponseEntity.status(HttpStatus.OK).body(userProfile);
+    //     } else {
+    //         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("Invalid token"));
+    //     }
+    // }
 
-    private TokenResponse refreshAccessToken(String refreshToken) throws IOException, GeneralSecurityException {
-        TokenResponse tokenResponse = new GoogleRefreshTokenRequest(
-                new NetHttpTransport(),
-                new JacksonFactory(),
-                refreshToken,
-                clientId,
-                clientSecret)
-                .execute();
+    // private TokenResponse refreshAccessToken(String refreshToken) throws IOException, GeneralSecurityException {
+    //     TokenResponse tokenResponse = new GoogleRefreshTokenRequest(
+    //             new NetHttpTransport(),
+    //             new JacksonFactory(),
+    //             refreshToken,
+    //             clientId,
+    //             clientSecret)
+    //             .execute();
 
-        return tokenResponse;
-    }
+    //     return tokenResponse;
+    // }
 }
